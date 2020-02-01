33ed487bc93280fd9dbf66000f93338a87db1bb7
Gareth Francis <gfrancis.dev@gmail.com>
2020-02-01 19:26:05 +0000

    CMake stuff, web page with sliders calling through to C++

--- webGLTest/run.sh
+++ webGLTest/run.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
 
-cd build
 python -m SimpleHTTPServer 8080
-cd -
 
--- webGLTest/src/main.cpp
+++ webGLTest/src/main.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <SDL.h>
-#include <GLES2/gl2.h>
+#include <GLES3/gl3.h>
 
 #ifdef __EMSCRIPTEN__
-#include <emscripten.h>
-#include <emscripten/fetch.h>
+# include <emscripten.h>
+# include <emscripten/fetch.h>
+# include <emscripten/bind.h>
 #endif
 
 #include <iostream>
@@ -30,6 +31,14 @@ GLfloat windowHeight = 600;
 SDL_Surface *screen = nullptr;
 static SDL_GLContext gl_context;
 
+glm::vec4 diffuseColour(1.f,1.f,1.f,1.f);
+
+glm::vec3 viewRot(1.0f);
+glm::vec3 viewRotDelta(1.0f,1.0f,1.0f);
+
+std::chrono::time_point<std::chrono::high_resolution_clock> startTime;
+std::chrono::time_point<std::chrono::high_resolution_clock> lastFrameTime;
+
 struct FetchData {
 	std::string data;
 	bool ready = false;
@@ -37,42 +46,58 @@ struct FetchData {
 
 #ifdef __EMSCRIPTEN__
   std::map<emscripten_fetch_t*, FetchData> currentFetches;
+  
+  // Function hooks to javascript
+	void webgltest_jshook_slider_colour_red( int v ) { diffuseColour.r = static_cast<float>(v) / 255.f; }
+	void webgltest_jshook_slider_colour_green( int v ) { diffuseColour.g = static_cast<float>(v) / 255.f; }
+	void webgltest_jshook_slider_colour_blue( int v ) { diffuseColour.b = static_cast<float>(v) / 255.f; }
+	void webgltest_jshook_slider_colour_alpha( int v ) { diffuseColour.a = static_cast<float>(v) / 255.f; }
+	
+  EMSCRIPTEN_BINDINGS(webGLTest) {
+		emscripten::function("slider_colour_red", &webgltest_jshook_slider_colour_red);
+		emscripten::function("slider_colour_green", &webgltest_jshook_slider_colour_green);
+		emscripten::function("slider_colour_blue", &webgltest_jshook_slider_colour_blue);
+		emscripten::function("slider_colour_alpha", &webgltest_jshook_slider_colour_alpha);
+  }
+  
 #endif
 
 GLuint vertexBuffer, shaderProgram;
 GLint shaderAttribute_vertCoord;
 GLint shaderUniform_mvp;
+GLint shaderUniform_diffuseColour;
 
 struct Vertex {
 	GLfloat x;
 	GLfloat y;
+	GLfloat z;
 };
 std::vector<Vertex> vertices;
 
 std::string shaderSource_Vertex = 
-"#version 100\n"
+"#version 300 es\n"
 "uniform mat4 mvp;\n"
-"attribute vec2 vertCoord;\n"
+"in vec3 vertCoord;\n"
 "void main(void) { \n"
-"gl_Position = mvp * vec4(vertCoord.x, vertCoord.y, 0.5, 1.0);\n"
+"gl_Position = mvp * vec4(vertCoord, 1.0);\n"
 "}\n"
 "";
 
 std::string shaderSource_Fragment =
-"#version 100\n"
+"#version 300 es\n"
+"uniform mediump vec4 diffuseColour;\n"
+"out mediump vec4 fragColour;\n"
 "void main(void) {\n"
-"gl_FragColor = vec4(1.0,0.0,0.0,1.0);\n"
+"fragColour = diffuseColour;\n"
 "}\n"
 "";
 
-std::list<std::string> shapes = {
+std::list<std::string> shapes;/* = {
   "110m_physical%2fne_110m_geography_regions_polys.shp",
   "110m_physical%2fne_110m_land.shp",
-};
+};*/
 
 std::string currentShape;
-
-float viewRot(0.0);
 
 #ifdef __EMSCRIPTEN__
 void downloadSucceeded(emscripten_fetch_t* fetch) {
@@ -106,16 +131,57 @@ void downloadProgress(emscripten_fetch_t* fetch) {
 }
 #endif
 
+void checkErrorCompileShader(GLint shader) {
+	GLint compileSuccess = 0;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &compileSuccess);
+	if( compileSuccess == GL_FALSE ) {
+		GLint logLength = 0;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLength);
+		std::vector<GLchar> log(static_cast<std::vector<GLchar>::size_type>(logLength));
+		glGetShaderInfoLog(shader, logLength, &logLength, &log[0]);
+		glDeleteShader(shader);
+
+		std::cout << "Failed to link shader: " << std::string(reinterpret_cast<const char*>(&log[0])) << std::endl;		
+	}
+}
+
 void initialiseGLData() {
 	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glFrontFace(GL_CCW);
+	glCullFace(GL_BACK);
 	glClearColor( 0.2f, 0.2f, 0.2f, 1.0f );
 	
 	// Vertex data TODO: Placeholder triangle
 	{
 		vertices = {
-			{-0.8f,-0.8f},
-			{ 0.8f,-0.8f},
-			{ 0.8f, 0.8f},
+			// bottom
+			/*{-1.0,-1.0,-1.0},
+			{1.0,-1.0,-1.0},
+			{-1.0,-1.0,1.0},
+			{-1.0,-1.0,1.0},
+			{1.0,-1.0,-1.0},
+			{1.0,-1.0,1.0},*/
+			// top
+			{-1.0,1.0,1.0},
+			{1.0,1.0,1.0},
+			{-1.0,1.0,-1.0},
+			{-1.0,1.0,-1.0},
+			{1.0,1.0,1.0},
+			{1.0,1.0,-1.0},
+			// left
+			{-1.0,1.0,-1.0},
+			{-1.0,-1.0,-1.0},
+			{-1.0,1.0,1.0},
+			{-1.0,1.0,1.0},
+			{-1.0,-1.0,-1.0},
+			{-1.0,-1.0,1.0},
+			// right
+			
+			
+			
 		};
 		glGenBuffers(1, &vertexBuffer);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
@@ -124,15 +190,19 @@ void initialiseGLData() {
 	
 	// Shader program
 	{
+		std::cout << "Compiling vertex shader" << std::endl;
 		auto vertShader = glCreateShader(GL_VERTEX_SHADER);
 		const char* vsource = shaderSource_Vertex.c_str();
 		glShaderSource(vertShader, 1, &vsource, nullptr);
 		glCompileShader(vertShader);
+		checkErrorCompileShader(vertShader);
 		
+		std::cout << "Compiling fragment shader" << std::endl;
 		auto fragShader = glCreateShader(GL_FRAGMENT_SHADER);
 		const char* fsource = shaderSource_Fragment.c_str();
 		glShaderSource(fragShader, 1, &fsource, nullptr);
 		glCompileShader(fragShader);
+		checkErrorCompileShader(fragShader);
 		
 		shaderProgram = glCreateProgram();
 		glAttachShader(shaderProgram, vertShader);
@@ -141,9 +211,10 @@ void initialiseGLData() {
 		
 		shaderAttribute_vertCoord = glGetAttribLocation(shaderProgram, "vertCoord");
 		glEnableVertexAttribArray(shaderAttribute_vertCoord);
-		glVertexAttribPointer(shaderAttribute_vertCoord, 2, GL_FLOAT, false, sizeof(Vertex), static_cast<const void*>(0));
+		glVertexAttribPointer(shaderAttribute_vertCoord, 3, GL_FLOAT, false, sizeof(Vertex), static_cast<const void*>(0));
     
     shaderUniform_mvp = glGetUniformLocation(shaderProgram, "mvp");
+    shaderUniform_diffuseColour = glGetUniformLocation(shaderProgram, "diffuseColour");
 	}
 }
 
@@ -156,7 +227,16 @@ void endApplication() {
 #endif
 
 void render() {
-
+	auto now = std::chrono::high_resolution_clock::now();
+	
+	float renderDelta = static_cast<float>(std::chrono::duration_cast<std::chrono::microseconds>(now - lastFrameTime).count()) / 1e6f;
+	lastFrameTime = now;
+	
+	//auto secsSinceStart = static_cast<float>(std::chrono::duration_cast<std::chrono::milliseconds>(now - startTime).count()) / 1000.0f;
+	//auto renderDelta = secsSinceStart - lastFrameTime;
+	//std::cout << "GAH!: " << renderDelta << std::endl;
+	//lastFrameTime = static_cast<float>(std::chrono::duration_cast<std::chrono::milliseconds>(now - startTime).count());
+	
 #ifndef __EMSCRIPTEN__
   // Get the next event
   SDL_Event event;
@@ -168,16 +248,18 @@ void render() {
     }
   }
 #endif
-
-
-  // viewRot += 0.01;
-
-  if( viewRot > 2 * 3.14 ) viewRot = 0.0;
+  
+  viewRot += renderDelta * viewRotDelta;
+  if( viewRot.x > 2 * 3.14 ) viewRot.x = 0.0f;
+  if( viewRot.y > 2 * 3.14 ) viewRot.y = 0.0f;
+  if( viewRot.z > 2 * 3.14 ) viewRot.z = 0.0f;
   
   // glm::mat4 projMat = glm::ortho( -180.0f, 180.0f, -90.0f, 90.0f, 1.f, -1.f );
   glm::mat4 projMat = glm::perspective( 90.0f, static_cast<GLfloat>(windowWidth) / static_cast<GLfloat>(windowHeight), 0.1f, 100.0f );
   glm::mat4 viewMat = glm::lookAt( glm::vec3(0.0, 0.0, 2.0), glm::vec3(0.0,0.0,0.0), glm::vec3(0.0,1.0,0.0) );
-  viewMat = glm::rotate( viewMat, viewRot, glm::vec3(0.0, 0.0, 1.0) );
+  viewMat = glm::rotate( viewMat, viewRot.x, glm::vec3(1.0, 0.0, 0.0) );
+  viewMat = glm::rotate( viewMat, viewRot.y, glm::vec3(0.0, 1.0, 0.0) );
+  viewMat = glm::rotate( viewMat, viewRot.z, glm::vec3(0.0, 0.0, 1.0) );
   auto mvp = projMat * viewMat /* * modelMat */; 
   
   SDL_GL_MakeCurrent(window, gl_context);
@@ -187,6 +269,7 @@ void render() {
   
   glUseProgram(shaderProgram);
   glUniformMatrix4fv(shaderUniform_mvp, 1, GL_FALSE, glm::value_ptr(mvp));
+  glUniform4fv(shaderUniform_diffuseColour, 1, glm::value_ptr(diffuseColour));
   
   glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
   glDrawArrays(GL_TRIANGLES, 0, vertices.size());
@@ -251,47 +334,62 @@ void render() {
 	// that grabs new data when needed or something
 	if( loopIter < 100 ) return;
   
-	auto nextShapeToLoad = std::find(shapes.begin(), shapes.end(), currentShape);
-	if( nextShapeToLoad == shapes.end() ) nextShapeToLoad = shapes.begin();
-	else if( ++nextShapeToLoad == shapes.end() ) nextShapeToLoad = shapes.begin();
-	currentShape = *nextShapeToLoad;
-	loopIter = 0;
-  
-  emscripten_fetch_attr_t attr;
-  emscripten_fetch_attr_init(&attr);
-  strcpy(attr.requestMethod, "GET");
-  attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY;
-  attr.onsuccess = downloadSucceeded;
-  attr.onerror = downloadFailed;
-  attr.onprogress = downloadProgress;
-  std::string url = "http://localhost:3000/data/";
-  url.append(currentShape);	
-  currentFetches[ emscripten_fetch(&attr, url.c_str())] = {};
+  if( !shapes.empty() ) {
+		auto nextShapeToLoad = std::find(shapes.begin(), shapes.end(), currentShape);
+		if( nextShapeToLoad == shapes.end() ) nextShapeToLoad = shapes.begin();
+		else if( ++nextShapeToLoad == shapes.end() ) nextShapeToLoad = shapes.begin();
+		currentShape = *nextShapeToLoad;
+		loopIter = 0;
+		
+		emscripten_fetch_attr_t attr;
+		emscripten_fetch_attr_init(&attr);
+		strcpy(attr.requestMethod, "GET");
+		attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY;
+		attr.onsuccess = downloadSucceeded;
+		attr.onerror = downloadFailed;
+		attr.onprogress = downloadProgress;
+		std::string url = "http://localhost:3000/data/";
+		url.append(currentShape);	
+		currentFetches[ emscripten_fetch(&attr, url.c_str())] = {};
+	}
 #endif
 }
 
 
 int main(int argc, char** argv) {
-  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+	
+	SDL_Init(SDL_INIT_VIDEO);
+	
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+	
+  // SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
   SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+#ifdef __EMSCRIPTEN__  
   SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 2);
+#else  
+  SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+#endif
 
-  SDL_Init(SDL_INIT_VIDEO);
   SDL_Renderer* renderer = nullptr;
   SDL_CreateWindowAndRenderer(windowWidth, windowHeight, SDL_WINDOW_OPENGL, &window, &renderer);
 
-  // TODO: Context flags
   gl_context = SDL_GL_CreateContext(window);
   
-  currentShape = *shapes.begin();
+	auto glVer = glGetString(GL_VERSION);
+	std::cout << "OpenGL Version: " << glVer << std::endl;
+  
+  if( !shapes.empty() ) currentShape = *shapes.begin();
 
   initialiseGLData();
+
+	startTime = std::chrono::high_resolution_clock::now();
 
 // Fire the main loop. Won't quit this until SDL does in some form
 #ifdef __EMSCRIPTEN__
   // void emscripten_set_main_loop(em_callback_func func, int fps, int simulate_infinite_loop);
-  emscripten_set_main_loop(render, 30, 1);
+  emscripten_set_main_loop(render, 60, 1);
 #else
   while( true ) {
 	  render();
