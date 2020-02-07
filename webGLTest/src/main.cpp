b6c47afee165697a35b7eba445b79f2f4af901b7
Gareth Francis <gfrancis.dev@gmail.com>
2020-02-07 23:44:19 +0000

    Setup triple light test scene

--- webGLTest/src/main.cpp
+++ webGLTest/src/main.cpp
@@ -327,13 +327,29 @@ int main(int argc, char** argv) {
 
   shaderDiffuse.reset(new Shader_diffuse());
   Light testLight1;
-  testLight1.position = {3.0f,3.0f,3.0f};
-  testLight1.colour = {0.8f,0.5f,0.5f};
+  testLight1.position = {-3.0f,-3.0f,2.0f};
+  testLight1.colour = {0.8f,0.1f,0.1f};
   testLight1.intensity = {0.1f,1.0f,0.4f};
   testLight1.falloff = 100.0f;
   testLight1.radius = 10.0f;
   shaderDiffuse->lights().emplace_back( testLight1 );
-    
+  
+  Light testLight2;
+  testLight2.position = {3.0f,-3.0f,2.0f};
+  testLight2.colour = {0.1f,0.8f,0.1f};
+  testLight2.intensity = {0.1f,1.0f,0.4f};
+  testLight2.falloff = 100.0f;
+  testLight2.radius = 10.0f;  
+  shaderDiffuse->lights().emplace_back( testLight2 );
+
+  Light testLight3;
+  testLight3.position = {0.0f,3.0f,2.0f};
+  testLight3.colour = {0.1f,0.1f,0.8f};
+  testLight3.intensity = {0.1f,1.0f,0.4f};
+  testLight3.falloff = 100.0f;
+  testLight3.radius = 10.0f;  
+  shaderDiffuse->lights().emplace_back( testLight3 );
+  
   renderer->shaders()["diffuse"] = shaderDiffuse;
   
   nullDiffuse.reset(new Texture_SDL2Image("data/textures/diffuse/null.png"));
